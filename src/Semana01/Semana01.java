fb26ba0de0be1a65f4cccc532b917cfe47b62b51
Gerd2108 <gerard.ra.mt@gmail.com>
2025-09-26 20:31:09 -0500

    Semana 01

--- src/Semana01/Semana01.java
+++ src/Semana01/Semana01.java
@@ -1,4 +1,3 @@
-
 package Semana01;
 
 public class Semana01 {
@@ -6,6 +5,8 @@ public class Semana01 {
    
     public static void main(String[] args) {
         System.out.println("Hola amigo del mundo!");
+        
+        System.out.println("Vale vale :s");
     }
     
 }
